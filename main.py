49cb66ad10c68202aec89d9a487064c370171588
Fernando Leite <fernandogleite@gmail.com>
2022-08-22 14:12:00 -0300

    Adição de jogada randomica do computador

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from show.tic_tac_toe import printBoard
 from verifiers.tic_tac_toe import verify_win
+
+from random import choice
+
+
 board = {'7': ' ', '8': ' ', '9': ' ',
          '4': ' ', '5': ' ', '6': ' ',
          '1': ' ', '2': ' ', '3': ' '}
+
+choiceds = []
+
+def random_choice(excluded_choices):
+    choices = [k for k in board.keys() if k not in excluded_choices]
+    return choice(choices)
 
 
 def run():
@@ -12,18 +22,24 @@ def run():
 
         print(f"A vez é do {turn}")
         printBoard(board)
+        
+        if turn == 'X':
+            choice = input("Informe qual casa você quer marcar: ")
 
-        choice = input("Informe qual casa você quer marcar: ")
+            if not (choice in board.keys()):
+                print('Escolha uma opção válida')
+                continue
 
-        if not (choice in board.keys()):
-            print('Escolha uma opção válida')
-            continue
-
-        if board[choice] != " ":
-            print('Escolha uma opção válida')
-            continue
-
+            if board[choice] != " ":
+                print('Escolha uma opção válida')
+                continue
+        
+        else:
+            choice = random_choice(choiceds)
+        
         board[choice] = turn
+        choiceds.append(choice)
+            
 
         if verify_win(board):
             printBoard(board)
